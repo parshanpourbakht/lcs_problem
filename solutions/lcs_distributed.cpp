73f5e4851f12b658cdad8c96d042eb4c55a13a13
Parshanl <>
2024-11-24 22:35:18 -0800

    row based decomposition

--- solutions/lcs_distributed.cpp
+++ solutions/lcs_distributed.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 #include <vector>
 #include <string>
 #include <algorithm>
@@ -8,162 +7,137 @@
 
 #include <mpi.h>
 
-#define DEFAULT_FIRST_STRING "AJBFAFA"
+#define DEFAULT_FIRST_STRING "AAACD"
 #define DEFAULT_SECOND_STRING "ABA"
 
-using MemoizedTable = std::vector<std::vector<int>>;
+using MemoizedTable = std::vector<std::vector<int> >;
 
-MemoizedTable initialize_table(int numRows, int numCols) {
-    return MemoizedTable(numRows + 1, std::vector<int>(numCols + 1, 0));
+MemoizedTable 
+initialize_table(int numRows, 
+                 int numCols){
+
+  return MemoizedTable(numRows + 1, std::vector<int>(numCols + 1, 0)); 
 }
 
 void 
-wavefront_worker(MemoizedTable& lcsTable, 
-                 const std::string& str1, 
-                 const std::string& str2, 
-                 int numRows, 
-                 int numCols, 
-                 int diag) {
+lcs(const std::string &str1, 
+         const std::string &str2, 
+         int startRow, 
+         int endRow, 
+         MemoizedTable &lcsTable) {
 
-    int rowStart = std::max(1, diag - numCols);
-    int rowEnd = std::min(numRows, diag - 1);
-
-    for (int row = rowStart; row <= rowEnd; ++row) {
-        int col = diag - row;
-        if (str1[row - 1] == str2[col - 1]) {
-            lcsTable[row][col] = lcsTable[row - 1][col - 1] + 1;
-        } else {
-            lcsTable[row][col] = std::max(lcsTable[row - 1][col], lcsTable[row][col - 1]);
-        }
+  int numRows = str1.size();
+  int numCols = str2.size();
+    
+  for (int row = startRow; row < endRow; ++row) {
+    for (int col = 1; col <= numCols; ++col) {
+      if (str1[row - 1] == str2[col - 1]) {
+        lcsTable[row][col] = lcsTable[row - 1][col - 1] + 1;
+      } else {
+        lcsTable[row][col] = std::max(lcsTable[row - 1][col], lcsTable[row][col - 1]);
+      }
     }
+  }
 }
 
 std::string 
-backtrack_lcs(const MemoizedTable& lcsTable, 
-              const std::string& str1, 
-              const std::string& str2, 
-              int numRows, 
-              int numCols) {
+backtrack_lcs(const std::string &str1, 
+              const std::string &str2, 
+              const MemoizedTable &lcsTable) {
 
-    std::string lcs;
-    int row = numRows, col = numCols;
-    while (row > 0 && col > 0) {
-        if (str1[row - 1] == str2[col - 1]) {
-            lcs += str1[row - 1];
-            row--;
-            col--;
-        } else if (lcsTable[row - 1][col] >= lcsTable[row][col - 1]) {
-            row--;
-        } else {
-            col--;
-        }
+  int row = str1.size();
+  int col = str2.size();
+  std::string lcs;
+
+  while (row > 0 && col > 0) {
+    if (str1[row - 1] == str2[col - 1]) {
+      lcs.push_back(str1[row - 1]);
+      row--;
+      col--;
+    } else if (lcsTable[row - 1][col] > lcsTable[row][col - 1]) {
+      row--;
+    } else {
+      col--;
     }
-    std::reverse(lcs.begin(), lcs.end());
+  }
 
-    return lcs;
-}
-
-std::string 
-lcs_distributed(const std::string& str1, 
-                const std::string& str2, 
-                int numRows, 
-                int numCols, 
-                double& serialTime, 
-                int rank, 
-                int size) {
-
-    timer threadTimer;
-    threadTimer.start();
-
-    MemoizedTable lcsTable = initialize_table(numRows, numCols);
-    int diagStart = rank + 1;
-    int diagEnd = numRows + numCols;
-
-    int edgeSize = std::max(numRows, numCols) + 1;
-    std::vector<int> sendBuffer(edgeSize, 0), recvBuffer(edgeSize, 0);
-
-    // Process the diagonals in round-robin 
-    for (int diag = diagStart; diag <= diagEnd; diag += size) {
-        // Receive data from the previous process 
-        if (diag > 1 && rank != 0) {
-            MPI_Recv(recvBuffer.data(), edgeSize, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            // Update the LCS table based on the received data
-            for (int i = 1; i <= edgeSize; ++i) {
-                if (i <= numRows && diag - i <= numCols && diag - i >= 1) {
-                    lcsTable[i][diag - i] = recvBuffer[i - 1];
-                }
-            }
-        }
-
-        // Compute the values for the current diagonal
-        wavefront_worker(lcsTable, str1, str2, numRows, numCols, diag);
-
-        // Send data to the next process
-        if (rank != size - 1) {
-            for (int i = 1; i <= edgeSize; ++i) {
-                if (i <= numRows && diag - i <= numCols && diag - i >= 1) {
-                    sendBuffer[i - 1] = lcsTable[i][diag - i];
-                }
-            }
-            MPI_Send(sendBuffer.data(), edgeSize, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
-        }
-    }
-
-    serialTime = threadTimer.stop();
-
-    // Debugging output prints out table values
-    // std::cout << "LCS Rank: " << rank << std::endl;
-    // for (int i = 0; i <= numRows; ++i) {
-    //     for (int j = 0; j <= numCols; ++j) {
-    //         std::cout << lcsTable[i][j] << " ";
-    //     }
-    //     std::cout << "\n";
-    // }
-
-    if (rank == 0) {
-        return backtrack_lcs(lcsTable, str1, str2, numRows, numCols);
-    }
-    return "";
+  std::reverse(lcs.begin(), lcs.end());
+  return lcs;
 }
 
 int 
-main(int argc, 
-     char* argv[]) {
+main(int argc, char** argv) {
+  MPI_Init(&argc, &argv);
 
-    MPI_Init(&argc, &argv);
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+  cxxopts::Options options("lcs_calculation",
+                           "Calculate longest common string between 2 strings");
+  options.add_options(
+      "custom",
+      {
+          {"str1", "First String",         
+           cxxopts::value<std::string>()->default_value(DEFAULT_FIRST_STRING)},
+          {"str2", "Second String", 
+           cxxopts::value<std::string>()->default_value(DEFAULT_SECOND_STRING)}
+      });
+  
+  auto cl_options = options.parse(argc, argv);
+  
+  std::string str1 = cl_options["str1"].as<std::string>();
+  std::string str2 = cl_options["str2"].as<std::string>();
 
-    cxxopts::Options options("lcs_calculation", "Calculate longest common subsequence between two strings");
-    options.add_options(
-        "custom",
-        {
-            {"str1", "First String", cxxopts::value<std::string>()->default_value(DEFAULT_FIRST_STRING)},
-            {"str2", "Second String", cxxopts::value<std::string>()->default_value(DEFAULT_SECOND_STRING)}
-        });
+  // str1 = "AGGTABA"; 
+  // str2 = "GXTXAYBGXTXAYBGXTXAYBGX"; 
 
-    auto cl_options = options.parse(argc, argv);
-    std::string str1 = cl_options["str1"].as<std::string>();
-    std::string str2 = cl_options["str2"].as<std::string>();
+  int numRows = str1.length();
+  int numCols = str2.length(); 
 
-    int numRows = str1.length();
-    int numCols = str2.length();
-    double serialTime = 0;
+  // LCS table
+  MemoizedTable lcsTable = initialize_table(numRows, numCols);
 
-    std::string lcsValue = lcs_distributed(str1, str2, numRows, numCols, serialTime, rank, size);
+  int processRows = (numRows + size - 1) / size;
+  int startRow = rank * processRows + 1; 
+  int endRow = std::min((rank + 1) * processRows + 1, numRows + 1);
 
-    if (rank == 0) {
-        std::cout << "Distributed Implementation" << std::endl;
-        std::cout << "---------------------------" << std::endl;
-        std::cout << "str1: " << str1 << std::endl;
-        std::cout << "str2: " << str2 << std::endl;
-        std::cout << "LCS: " << lcsValue << std::endl;
-        std::cout << "Time (seconds): " << serialTime << std::endl;
+  // Synchronize with the last row of the previous process
+  if (rank > 0) {
+    MPI_Recv(lcsTable[startRow - 1].data(), numCols + 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
 
+  // Compute segment of the LCS table
+  lcs(str1, str2, startRow, endRow, lcsTable);
+
+  // Send the last row of this segment to the next process
+  if (rank < size - 1) {
+    MPI_Send(lcsTable[endRow - 1].data(), numCols + 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+  }
+
+  // Gather the computed segments back to process 0
+  if (rank == 0) {
+    for (int p = 1; p < size; ++p) {
+      int processStartRow = p * processRows + 1;
+      int processEndRow = std::min((p + 1) * processRows + 1, numRows + 1);
+
+      for (int row = processStartRow; row < processEndRow; ++row) {
+        MPI_Recv(lcsTable[row].data(), numCols + 1, MPI_INT, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      }
     }
 
-    MPI_Finalize();
-    return 0;
+    // Calculate the LCS string
+    std::string lcs = backtrack_lcs(str1, str2, lcsTable);
+
+    // Print the LCS length and string
+    std::cout << "LCS Length: " << lcsTable[numRows][numCols] << std::endl;
+    std::cout << "LCS String: " << lcs << std::endl;
+  } else {
+    for (int row = startRow; row < endRow; ++row) {
+      MPI_Send(lcsTable[row].data(), numCols + 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+  }
+
+  MPI_Finalize();
+  return 0;
 }
